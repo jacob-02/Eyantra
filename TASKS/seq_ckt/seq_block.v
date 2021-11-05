f3d00666afbbd4243d13e78f1b9017c83264a493
jacob-02 <jvsanoj23@gmail.com>
2021-11-05 23:11:34 +0530

    Updating for task B and D

---
+++ TASKS/seq_ckt/seq_block.v
@@ -0,0 +1,51 @@
+// Copyright (C) 2019  Intel Corporation. All rights reserved.
+// Your use of Intel Corporation's design tools, logic functions 
+// and other software and tools, and any partner logic 
+// functions, and any output files from any of the foregoing 
+// (including device programming or simulation files), and any 
+// associated documentation or information are expressly subject 
+// to the terms and conditions of the Intel Program License 
+// Subscription Agreement, the Intel Quartus Prime License Agreement,
+// the Intel FPGA IP License Agreement, or other applicable license
+// agreement, including, without limitation, that your use is for
+// the sole purpose of programming logic devices manufactured by
+// Intel and sold by Intel or its authorized distributors.  Please
+// refer to the applicable agreement for further details, at
+// https://fpgasoftware.intel.com/eula.
+
+// PROGRAM		"Quartus Prime"
+// VERSION		"Version 19.1.0 Build 670 09/22/2019 SJ Lite Edition"
+// CREATED		"Fri Nov 05 16:39:12 2021"
+
+module seq_block(
+	CLK,
+	Y
+);
+
+
+input wire	CLK;
+output wire	[2:0] Y;
+
+wire	[2:0] Q;
+wire	SYNTHESIZED_WIRE_0;
+wire	SYNTHESIZED_WIRE_1;
+wire	SYNTHESIZED_WIRE_2;
+wire	SYNTHESIZED_WIRE_3;
+
+
+
+
+assign	SYNTHESIZED_WIRE_0 = ;
+
+assign	SYNTHESIZED_WIRE_3 = ;
+
+
+assign	SYNTHESIZED_WIRE_2 = ;
+
+assign	SYNTHESIZED_WIRE_1 = ;
+
+
+
+assign	Y = Q;
+
+endmodule
